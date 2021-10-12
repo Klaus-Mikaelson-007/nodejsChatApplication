c243f81b46adc1c176dd1acefd1019fcbeca1e66
Klaus Mikaelson <88378423+Klaus-Mikaelson-007@users.noreply.github.com>
2021-10-12 15:37:15 +0530

    Added basic rooms functionality

--- app.js
+++ app.js
@@ -8,20 +8,28 @@ const io = require('socket.io')(server)
 app.use(express.static(__dirname+'/public'))
 
 var users ={};
+var contact;
+
 
 io.on('connection', socket =>{
     console.log('Connected');
+
+    socket.on('contact', contacts =>{
+        contact = contacts;
+        socket.join(contact);
+    })
+
     socket.on('new-user', name =>{
         users[socket.id] = name;
-        socket.broadcast.emit('user-connected', name)
+        io.sockets.in(contact).emit('user-connected', name)
     })
 
     socket.on('send-message', message =>{
-        socket.broadcast.emit('chat', {message: message, name:users[socket.id]})
+        socket.broadcast.to(contact).emit('chat', {message: message.message, name:users[socket.id], self:message.self})
     })
 
     socket.on('disconnect',()=>{
-        socket.broadcast.emit('disconnected', users[socket.id])
+        io.sockets.in(contact).emit('disconnected', users[socket.id])
         delete users[socket.id]
     })
 })
@@ -29,17 +37,6 @@ io.on('connection', socket =>{
 app.get("/", (req,res)=>{
     res.sendFile(__dirname+"index.html")
 })
-
-
-
-
-
-
-
-
-
-
-
 
 
 const PORT = process.env.PORT || 3000;
